7ac07a5da9f8ced30fdc26aab5091be810ee535a
狠心源 <50833540+2149495141@users.noreply.github.com>
2023-05-01 23:27:07 +0800

    Delete RTExperimentalIdeas directory

--- RTExperimentalIdeas/Material/Dispersion.java
+++
@@ -1,4 +0,0 @@
-package RTExperimentalIdeas.Material;
-
-public class Dispersion {
-}
--- RTExperimentalIdeas/Texture/Texture.java
+++
@@ -1,8 +0,0 @@
-package RTExperimentalIdeas.Texture;
-
-import RTExperimentalIdeas.Color;
-import RTExperimentalIdeas.Point;
-
-public interface Texture {
-    Color value(double u, double v, Point p);
-}
