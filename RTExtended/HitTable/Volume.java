5d09c01bd0a9c0ac0e303672129465d656c58bc7
狠心源 <50833540+2149495141@users.noreply.github.com>
2024-06-16 23:51:09 +0800

    Add files via upload

--- RTExtended/HitTable/Volume.java
+++ RTExtended/HitTable/Volume.java
@@ -5,73 +5,74 @@ import RTExtended.HitInfo.Interval;
 import RTExtended.Material.Isotropic;
 import RTExtended.Ray;
 import RTExtended.Color;
-import RTExtended.Material.Material;
-import RTExtended.Texture.Texture;
 import RTExtended.Vec3;
 
 import static RTExtended.RTUtil.random_double;
 import static RTExtended.RTUtil.infinity;
-import static java.lang.Math.abs;
+
 
 public class Volume implements HitTable {
     HitTable boundary;
     double neg_inv_density;
-    Material phase_function;
-    record volume_hit_info(double first_p, double distance){}
+    Color sigma_a, sigma_s, sigma_t;
+    record volumeHitInfo(double first_p, double distance, Vec3 tr){}
 
-    public Volume(HitTable b, double d, Color c){
+    public Volume(HitTable b, double d, Color a, Color s){
         boundary = b;
         neg_inv_density = -1/d;
-        phase_function = new Isotropic(c);
-    }
-    public Volume(HitTable b, double d, Texture t){
-        boundary = b;
-        neg_inv_density = -1/d;
-        phase_function = new Isotropic(t);
+        sigma_a = a;
+        sigma_s = s;
+        sigma_t = sigma_a.add(sigma_s);
     }
     
 
     @Override
     public boolean hit(Ray r, Interval t, HitRecord rec) {
-        volume_hit_info volumeInfo = getHitPointAndDistance(r, t, boundary);
+        volumeHitInfo volume_info = getVolumeHitInfo(r, t, boundary);
 
-        double ray_length = r.dir().length();
-        double distance_inside_boundary = volumeInfo.distance * ray_length;
+        double distance_inside_boundary = volume_info.distance * r.dir().length();
         double hit_distance = neg_inv_density * Math.log(random_double());
 
         if (hit_distance >= distance_inside_boundary)
                 return false;
 
-        rec.t = volumeInfo.first_p + hit_distance / ray_length;
+        rec.t = volume_info.first_p + hit_distance / r.dir().length();
         rec.p = r.at(rec.t);
-        rec.normal = new Vec3(1);
+        rec.normal = new Vec3(0);
         rec.front_face = true;
-        rec.mat = phase_function;
+        rec.mat = new Isotropic(sigma_a.add(sigma_s.multiply(volume_info.tr)));
 
         return true;
     }
 
-    volume_hit_info getHitPointAndDistance(Ray r, Interval t, HitTable boundary) {
-        HitRecord hit_p1 = new HitRecord(), hit_p2 = new HitRecord();
-        Interval tt = new Interval(-infinity,infinity);
-
+    volumeHitInfo getVolumeHitInfo(Ray r, Interval t, HitTable boundary) {
         boolean is_first = true;
         double first_point = 0;
         double distance = 0;
-        while (boundary.hit(r, tt, hit_p1)) {
+        Vec3 tr = new Vec3(1);
+
+        HitRecord hit_p1 = new HitRecord(), hit_p2 = new HitRecord();
+        Interval nxte_t = new Interval(-infinity, infinity);
+        while (boundary.hit(r, nxte_t, hit_p1)) {
             if (hit_p1.t < t.min()) hit_p1.t = t.min();
-            if (hit_p1.t < 0) hit_p1.t = 0;
-            tt = new Interval(hit_p1.t + 1e-5, infinity);
-            
-            if (is_first) { first_point = hit_p1.t; is_first = false; }
-            if (boundary.hit(r, tt, hit_p2)) {
-                if (hit_p2.t > t.max()) hit_p2.t = t.max();
-                if (hit_p1.t >= hit_p2.t) break;
-                tt = new Interval(hit_p2.t + 1e-5, infinity);
-                distance += hit_p2.t - hit_p1.t;
-            } else break;
+            if (hit_p1.t < 0)       hit_p1.t = 0;
+            if (is_first) {
+                first_point = hit_p1.t;
+                is_first = false;
+            }
+
+            nxte_t = new Interval(hit_p1.t + 1e-5, infinity);
+            if (boundary.hit(r, nxte_t, hit_p2)) {
+                if (hit_p2.t > t.max())     hit_p2.t = t.max();
+                if (hit_p1.t >= hit_p2.t)   break;
+
+                distance += (hit_p2.t - hit_p1.t);
+                tr.multiply_Equal(Vec3.Exp(sigma_t.negative().multiply(distance)));
+                nxte_t = new Interval(hit_p2.t + 1e-5, infinity);
+            }
+            else break;
         }
-        return new volume_hit_info(first_point, distance);
+        return new volumeHitInfo(first_point, distance, tr);
     }
 
     @Override
